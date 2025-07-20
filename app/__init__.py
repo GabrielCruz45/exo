13bd3d44ff6a83e6d4fd154dab1a7f1b3133ccdf
Gabriel Cruz <gabriel.crzprz45@gmail.com>
2025-07-20 03:08:50 -0400

    building blocks already set and fully execute

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,10 @@
 # Contains the Application Factory (create_app)
-from flask import Flask, Blueprint, render_template, abort
-from jinja2 import TemplateNotFound
-from extensions import db, migrate, socketio
-from .. import config
-# blueprint
-from main import routes
-
+from flask import Flask
+from .extensions import db, migrate, socketio
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_pyfile(config)
+    app.config.from_object('config.Config') # reads the variables from your config.py file and loads them into the application itself
 
     # initialize extensions
     db.init_app(app)
@@ -17,6 +12,8 @@ def create_app():
     migrate.init_app(app, db)
 
     # register blueprint
-    app.register_blueprint(routes.homepage)
+    with app.app_context():
+        from .main.__init__ import homepage_bp
+        app.register_blueprint(homepage_bp)
 
     return app
--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
-from run import app
+# from dotenv import load_dotenv
 
-class Config(app):
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# load_dotenv(os.path.join(basedir, '.env'))
+
+class Config():
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
