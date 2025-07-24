675c5a0ce39b21b7ec53efc18ec9022db5796ac1
Gabriel Cruz <gabriel.crzprz45@gmail.com>
2025-07-24 02:14:50 -0400

    added login_manager initialization; user_loader function;

--- app/extensions.py
+++ app/extensions.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from flask_migrate import Migrate
 from flask_socketio import SocketIO
+from flask_login import LoginManager
 
 class Base(DeclarativeBase):
   pass
@@ -13,3 +14,6 @@ db = SQLAlchemy(model_class=Base)
 migrate = Migrate()
 
 socketio = SocketIO()
+
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login' # if user tries to access protected pages without permission
