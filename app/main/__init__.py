358e49467946b50602202c3ebb3dd31a613359ef
Gabriel Cruz <gabriel.crzprz45@gmail.com>
2025-07-23 21:34:54 -0400

    started with app/auth/routes.py

--- app/main/__init__.py
+++
@@ -1,8 +0,0 @@
-# /main Blueprint definition
-from flask import Blueprint
-
-homepage_bp = Blueprint('homepage', __name__)
-
-# The reason it goes at the bottom is to avoid a circular import. 
-# You must define bp before you import the routes file that depends on bp.
-from . import routes
