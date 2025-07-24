8183d4e64698838928c3774cb572624bf94efe2b
Gabriel Cruz <gabriel.crzprz45@gmail.com>
2025-07-24 02:14:50 -0400

    added login_manager initialization; user_loader function;

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 # Contains the Application Factory (create_app)
 from flask import Flask
-from .extensions import db, migrate, socketio
+from .models import User
+
+from .extensions import db, migrate, socketio, login_manager
 
 def create_app():
     app = Flask(__name__)
@@ -10,6 +12,11 @@ def create_app():
     db.init_app(app)
     socketio.init_app(app)
     migrate.init_app(app, db)
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
     # register blueprint
     with app.app_context():
