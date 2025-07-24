3683b35ff6f1ada659d5ab1682e545b196a669d5
Gabriel Cruz <gabriel.crzprz45@gmail.com>
2025-07-24 03:48:04 -0400

    created decorators.py for @admin_required

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -1,1 +1,21 @@
 # Route for the user approval dashboard
+from flask import Blueprint, abort
+from flask_login import login_required
+
+from ..decorators import admin_required
+
+
+admin_bp = Blueprint(
+    'admin',
+    __name__,
+    template_folder='templates/admin',
+    url_prefix='/admin'
+)
+
+
+
+@admin_bp.route('/dashboard')
+@login_required
+@admin_required
+def admin_dashboard():
+    return
